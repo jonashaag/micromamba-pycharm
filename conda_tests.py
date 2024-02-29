895b3b468bc256482f6ae5c1fd20d7929cf8a8ca
pk5ls20 <pk5ls20@outlook.com>
2024-03-01 01:08:17 +0800

    Optimize the parametrization of `conda_tests.py`

--- conda_tests.py
+++ conda_tests.py
@@ -16,15 +16,17 @@ except NameError:
 SYSTEM = platform.system()
 
 
-def run_conda(args, use_bat):
-    print("use_bat: ", use_bat)
-    executable = ["cmd", "/c"] if (use_bat and SYSTEM == 'Windows') else [sys.executable]
-    exec_file = "conda.bat" if (use_bat and SYSTEM == 'Windows') else "conda"
+def run_conda(args, test_platform):
+    executable = ["cmd", "/c"] if SYSTEM == 'Windows' else [sys.executable]
+    exec_file = "conda.bat" if SYSTEM == 'Windows' else "conda"
     commend = executable + [exec_file] + args
-    if use_bat and SYSTEM == 'Windows':
+    if_windows = SYSTEM == 'Windows'
+    if_unix = SYSTEM in ("Linux", "Darwin")
+    if test_platform == 'Windows' and if_windows:
         call_generate_bat_file(None, None)
-    if use_bat and SYSTEM != 'Windows':
-        pytest.skip("use_bat test only available in Windows!")
+    if (test_platform == 'Windows' and if_unix) or (test_platform != 'Windows' and if_windows):
+        pytest.skip("The expected test platform {} is inconsistent with the current test platform {}, "
+                    "skip the test").format(test_platform, SYSTEM)
     return (
         subprocess.check_output(commend)
         .decode(locale.getpreferredencoding())
@@ -33,15 +35,15 @@ def run_conda(args, use_bat):
 
 
 @pytest.fixture(autouse=True)
-@pytest.mark.parametrize("use_bat", [False, True])
-def setup_PATH(monkeypatch, use_bat):
+@pytest.mark.parametrize("test_platform", ["Windows", "Unix"])
+def setup_PATH(monkeypatch, test_platform):
     monkeypatch.setenv("PATH", os.getcwd() + (":" if SYSTEM != 'Windows' else ';') + os.environ["PATH"])
-    run_conda(["self-check"], use_bat)
+    run_conda(["self-check"], test_platform)
 
 
-@pytest.mark.parametrize("use_bat", [False, True])
-def test_info_envs_json(use_bat):
-    result = run_conda(["info", "--envs", "--json"], use_bat)
+@pytest.mark.parametrize("test_platform", ["Windows", "Unix"])
+def test_info_envs_json(test_platform):
+    result = run_conda(["info", "--envs", "--json"], test_platform)
     data = json.loads(result)
     assert isinstance(data, dict)
     assert set(data.keys()) == {"envs_dirs", "conda_prefix", "envs"}
@@ -52,9 +54,9 @@ def test_info_envs_json(use_bat):
     assert all(isinstance(item, unicode) for item in data["envs"])
 
 
-@pytest.mark.parametrize("use_bat", [False, True])
-def test_env_list_json(use_bat):
-    result = run_conda(["env", "list", "--json"], use_bat)
+@pytest.mark.parametrize("test_platform", ["Windows", "Unix"])
+def test_env_list_json(test_platform):
+    result = run_conda(["env", "list", "--json"], test_platform)
     data = json.loads(result)
     assert isinstance(data, dict)
     assert set(data.keys()) == {"envs"}
@@ -62,23 +64,23 @@ def test_env_list_json(use_bat):
     assert all(isinstance(item, unicode) for item in data["envs"])
 
 
-@pytest.mark.parametrize("use_bat", [False, True])
-def test_list(use_bat):
-    result = run_conda(["list", "-n", "base"], use_bat)
-    result_e = run_conda(["list", "-n", "base", "-e"], use_bat)
+@pytest.mark.parametrize("test_platform", ["Windows", "Unix"])
+def test_list(test_platform):
+    result = run_conda(["list", "-n", "base"], test_platform)
+    result_e = run_conda(["list", "-n", "base", "-e"], test_platform)
     ref = subprocess.check_output(["micromamba", "list", "-n", "base"]).strip()
     ref = "\n".join(l.strip() for l in ref.splitlines()[4:])
     assert result == ref
     assert result_e == "\n".join("=".join(l.split()[:-1]) for l in ref.splitlines())
 
 
-@pytest.mark.parametrize("use_bat", [False, True])
-def test_run(use_bat):
-    assert "42" == run_conda(["run", "echo", "42"], use_bat)
-    assert "42" == run_conda(["run", "--no-capture-output", "echo", "42"], use_bat)
+@pytest.mark.parametrize("test_platform", ["Windows", "Unix"])
+def test_run(test_platform):
+    assert "42" == run_conda(["run", "echo", "42"], test_platform)
+    assert "42" == run_conda(["run", "--no-capture-output", "echo", "42"], test_platform)
 
 
-@pytest.mark.parametrize("use_bat", [False, True])
-def test_create(tmp_path, use_bat):
-    run_conda(["create", "-p", str(tmp_path / "env")], use_bat)
-    run_conda(["install", "-p", str(tmp_path / "env"), "xtensor"], use_bat)
+@pytest.mark.parametrize("test_platform", ["Windows", "Unix"])
+def test_create(tmp_path, test_platform):
+    run_conda(["create", "-p", str(tmp_path / "env")], test_platform)
+    run_conda(["install", "-p", str(tmp_path / "env"), "xtensor"], test_platform)
